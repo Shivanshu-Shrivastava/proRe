1b70d893ff07692f0feb7aadd647e4e7c6a21f06
Divyesh Mehta <divyeshhmehta13@gmail.com>
2021-03-17 19:03:40 +0530

    Add files via upload

--- resume_maker/views.py
+++ resume_maker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import *
 from .models import *
@@ -43,11 +43,11 @@ def render_page(request):
         "hobb": Hobbies,
     }
 
-    return render(request,"resume_maker\site.html",context)
+    return render(request,"site.html",context)
 
 
 def personal_det(request):
-    return render(request,"resume_maker\getinput.html", { "person":PersonForm})
+    return render(request,"getinput.html", { "person":PersonForm})
 
 first = True
 def get_input(request):
@@ -81,53 +81,95 @@ def get_input(request):
                 form.save()
 
             #form = PersonForm()
-            first = False
-            return render(request,"resume_maker/getinput.html",{"form":form,"person":form.cleaned_data,"show":first})
+            if first == True:
+                first = False
+            return render(request,"getinput.html",{"form":form,"person":form.cleaned_data,"show":first})
     else:
         form = PersonForm()
-    return render(request,"resume_maker/getinput.html",{"form":form,"show":first,"person":person})
+    return render(request, "getinput.html",{"form":form,"show":first,"person":person})
 
-def edu(request,id = None):
+def edu(request):
+    global first
     person = Person.objects.last()
-    show = True
+    #first = True
     edu= Education.objects.filter(person = person)
+    new_form = EducationForm()
+    """r = Education.objects.filter(person = person).first()
+    t = []
+    t.append(Education.objects.filter(person = person))"""
         #form = EducationForm(instance = )
     if request.method == "POST":
-        if id:
-            delete = Education.objects.get(id = id)
-            delete.delete()
+        form = EducationForm(request.POST or None)
+        if form.is_valid():
+            cd = form.cleaned_data
+            qualification = cd.get('qualification')
+            institution = cd.get('institution')
+            board = cd.get('board')
+            start_yr = cd.get('start_yr')
+            end_yr = cd.get('end_yr')
+            cgpa = cd.get('cgpa')
+            percent = cd.get('percent')
+            """if id:
+                edit = Education.objects.filter(id=id)
+                new_form = EducationForm(request.POST or None,instance=edit)
+                new_form.save()
+            else:"""
+            new_form = EducationForm()
+            data = Education(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
+            temp = Education.objects.filter(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
+            if temp.exists():
+                temp.update(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
+            else:
+                data.save()
         else:
-            form = EducationForm(request.POST or None)
-            if form.is_valid():
-                cd = form.cleaned_data
-                qualification = cd.get('qualification')
-                institution = cd.get('institution')
-                board = cd.get('board')
-                start_yr = cd.get('start_yr')
-                end_yr = cd.get('end_yr')
-                cgpa = cd.get('cgpa')
-                percent = cd.get('percent')
-
-                data = Education(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
-
-                temp = Education.objects.filter(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
-
-                if temp.exists():
-                    temp.update(qualification = qualification, institution = institution, board = board, start_yr = start_yr, end_yr = end_yr, cgpa = cgpa, percent = percent,person = person)
-                else:
-                    data.save()
+            new_form = EducationForm()
         form = EducationForm()
-        show = True
-        return render(request,"resume_maker\Education.html",{"edu":form,"data":edu,"show":show})
+        if first == True:
+            first = False
+        return render(request,"Education.html",{"edu":form,"data":edu,"show":first})
     else:
         form = EducationForm()
-    return render(request,"resume_maker\Education.html",{"edu":form,"data":edu,"show":show})
+    return render(request,"Education.html",{"edu":form,"data":edu,"show":first,"edit":new_form})
 
-def wor(request):
+def eduedit(request,id):
+    instance = Education.objects.get(id=id)
+    edit = Education.objects.filter(id = id)
+    print(instance)
+    if request.method == "POST":
+        edit_form = EducationForm(request.POST or None, instance = instance)
+        if edit_form.is_valid():
+            cd = edit_form.cleaned_data
+            qualification = cd.get('qualification')
+            institution = cd.get('institution')
+            board = cd.get('board')
+            start_yr = cd.get('start_yr')
+            end_yr = cd.get('end_yr')
+            cgpa = cd.get('cgpa')
+            percent = cd.get('percent')
+
+        data = Education(qualification=qualification, institution=institution, board=board, start_yr=start_yr, end_yr=end_yr, cgpa=cgpa, percent=percent, person=person)
+        edit.update(qualification=qualification, institution=institution, board=board, start_yr=start_yr, end_yr=end_yr, cgpa=cgpa, percent=percent, person=person)
+
+    else:
+        edit_form = EducationForm(instance = instance)
+
+    return redirect('/education')
+
+
+def edudel(request,id):
+    delete = Education.objects.filter(id=id)
+    delete.delete()
+    form = EducationForm()
+    new_form = EducationForm()
+    return redirect('/education')
+
+
+def wor(request,id=None):
     person = Person.objects.last()
     print(person)
     show = True
     exp = Experience.objects.filter(person = person)
+
     if request.method == "POST":
         form = ExperienceForm(request.POST)
         if form.is_valid():
@@ -148,16 +190,42 @@ def wor(request):
                 data.save()
 
             show = True
-
             form = ExperienceForm()
             print(exp)
-
-            return render(request,"resume_maker\work.html",{"exp":form,"data":exp,"show":show})
+            return render(request,"work.html",{"exp":form,"data":exp,"show":show})
     else:
         form = ExperienceForm()
-    return render(request,"resume_maker\work.html",{"exp" : form,"show":show,"data":exp})
+    return render(request,"work.html",{"exp" : form,"show":show,"data":exp})
 
-def skill(request):
+
+def editwork(request,id):
+    instance = Experience.objects.get(id=id)
+    edit = Experience.objects.filter(id=id)
+    print(instance)
+    if request.method == "POST":
+        edit_form = ExperienceForm(request.POST or None, instance=instance)
+        if edit_form.is_valid():
+            cd = edit_form.cleaned_data
+            type = cd.get('type')
+            company = cd.get('company')
+            role = cd.get('role')
+            join_dt = cd.get('join_dt')
+            left_dt = cd.get('left_dt')
+            details = cd.get('details')
+            data = Experience(type = type,company = company,role = role, join_dt = join_dt, left_dt = left_dt, details = details, person = person)
+            edit.update(type = type,company = company,role = role, join_dt = join_dt, left_dt = left_dt, details = details, person = person)
+
+    else:
+        edit_form = ExperienceForm(instance=instance)
+    return redirect('/work')
+
+def delwork(request,id):
+    delete = Experience.objects.filter(id=id)
+    delete.delete()
+    form = ExperienceForm()
+    return redirect('/work')
+
+def skill(request,id = None):
     show = True
     person = Person.objects.last()
     skills = SkillSet.objects.filter(person = person)
@@ -177,10 +245,37 @@ def skill(request):
 
             form = SkillsForm()
 
-            return render(request,"resume_maker\skillset.html",{"skill":form,"data":skills,"show":show})
+            return render(request,"skillset.html",{"skill":form,"data":skills,"show":show})
     else:
         form = SkillsForm()
-    return render(request,"resume_maker\skillset.html", { "skill": form,"show":show,"data":skills})
+    return render(request,"skillset.html", { "skill": form,"show":show,"data":skills})
+
+def editskill(request,id):
+    instance = SkillSet.objects.get(id=id)
+    edit = SkillSet.objects.filter(id=id)
+    print(instance)
+    if request.method == "POST":
+        edit_form = SkillsForm(request.POST or None, instance=instance)
+        if edit_form.is_valid():
+            cd = edit_form.cleaned_data
+            skill = cd.get('skill')
+            exp = cd.get('experience')
+            #show = True
+
+            data = SkillSet(skill = skill,experience = exp,person = person)
+            edit.update(skill = skill,experience = exp,person = person)
+
+    else:
+        edit_form = SkillsForm(instance=instance)
+
+    return redirect('/skills')
+
+
+def delskill(request,id):
+    delete = SkillSet.objects.filter(id=id)
+    delete.delete()
+    form = SkillsForm()
+    return redirect('/skills')
 
 def pro(request):
     show =True
@@ -203,27 +298,52 @@ def pro(request):
                 temp.update(project = project, start_dt = start_dt, end_dt = end_dt,running = running, project_link = project_link, person = person)
             else:
                 data.save()
-
             form = ProjectsForm()
-
             show = True
-
-            return render(request,"resume_maker\project.html",{"prjct":form,"data":prjcts,"show":show})
+            return render(request,"project.html",{"prjct":form,"data":prjcts,"show":show})
     else:
         form = ProjectsForm()
-    return render(request,"resume_maker\project.html", {"prjct": form,"show":show,"data":prjcts})
+    return render(request,"project.html", {"prjct": form,"show":show,"data":prjcts})
 
-def lang(request):
+def editpro(request,id):
+    instance = Projects.objects.get(id=id)
+    edit = Projects.objects.filter(id=id)
+    print(instance)
+    if request.method == "POST":
+        edit_form = ProjectsForm(request.POST or None, instance=instance)
+        if edit_form.is_valid():
+            cd = edit_form.cleaned_data
+            project = cd.get('project')
+            start_dt = cd.get('start_dt')
+            end_dt = cd.get('end_dt')
+            running = cd.get('running')
+            project_link = cd.get('project_link')
+
+            data = Projects(project = project, start_dt = start_dt, end_dt = end_dt,running = running, project_link = project_link, person = person)
+            edit.update(project = project, start_dt = start_dt, end_dt = end_dt,running = running, project_link = project_link, person = person)
+    else:
+        edit_form = ProjectsForm(instance=instance)
+
+    return redirect('/project')
+
+
+def delpro(request,id):
+    delete = Projects.objects.filter(id=id)
+    delete.delete()
+    form = ProjectsForm()
+    return redirect('/project')
+
+"""def lang(request):
     if request.method == "POST":
         form = LanguageForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request,"resume_maker\languages.html",{"lang":form,"data":form.cleaned_data})
+            return render(request,"languages.html",{"lang":form,"data":form.cleaned_data})
     else:
         form = LanguageForm()
-    return render(request,"resume_maker\languages.html", { "lang": form })
+    return render(request,"languages.html", { "lang": form })"""
 
-def ext(request):
+def ext(request,id = None):
     show = True
     person = Person.objects.last()
     ach = Achievements.objects.filter(person = person)
@@ -243,10 +363,34 @@ def ext(request):
 
             form = AchievementsForm()
 
-            return render(request,"resume_maker\extra.html",{"ach":form,"data":ach, "show":show})
+            return render(request,"extra.html",{"ach":form,"data":ach, "show":show})
     else:
         form = AchievementsForm()
-    return render(request,"resume_maker\extra.html", { "ach" : form, "show":show,"data":ach})
+    return render(request,"extra.html", { "ach" : form, "show":show,"data":ach})
+
+def editext(request,id):
+    instance = Achievements.objects.get(id=id)
+    edit = Achievements.objects.filter(id=id)
+    print(instance)
+    if request.method == "POST":
+        edit_form = AchievementsForm(request.POST or None, instance=instance)
+        if edit_form.is_valid():
+            cd = edit_form.cleaned_data
+            achievement = cd.get('achievement')
+
+            data = Achievements(achievement = achievement,person = person)
+            edit.update(achievement = achievement,person = person)
+    else:
+        edit_form = AchievementsForm(instance=instance)
+
+    return redirect('/extra')
+
+
+def delext(request,id):
+    delete = Achievements.objects.filter(id=id)
+    delete.delete()
+    form = AchievementsForm()
+    return redirect('/extra')
 
 def get_pdf(request):
     pass
@@ -269,6 +413,3 @@ def get_pdf(request):
     html = HTML(string = html_string)
 
     html.write_pdf(target = 'resume.pdf')"""
-
-
-
